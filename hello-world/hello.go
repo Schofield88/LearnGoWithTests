9c2cd771e9c78f0c7ece3552e3b79d6728b93920
Schofield88 <luke@fundapps.co>
2020-04-11 20:29:20 +0100

    More features, more tests, moar powa

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -10,21 +10,25 @@ const englishHelloPrefix = "Hello, "
 const frenchHelloPrefix = "Salut, "
 const czechHelloPrefix = "Cao, "
 
-func Hello(name, language string) string {
-	if name == "" {
-		name = "World"
-	}
-
-	prefix := englishHelloPrefix
-
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case czech:
 		prefix = czechHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
 
-	return prefix + name
+	return
+}
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
 }
 
 func main () {
